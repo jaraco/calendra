1df24fc1ecb83ec3c9ec474aabb90aeabfb6f12f
Renier de Bruyn <renierdebruyn@likeminds.co.za>
2014-11-11 15:04:43 +0200

    made PEP8 Compliant

--- calendra/africa.py
+++ calendra/africa.py
@@ -2,7 +2,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from .core import WesternCalendar
-from .core import SUN, SAT
 from .core import IslamicMixin, ChristianMixin
 
 
